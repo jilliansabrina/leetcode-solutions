72cbc6031b50234d388d3ee40af8e0ab4b72410c
Jillian Alvarez-Sanlley <64044770+jilliansabrina@users.noreply.github.com>
2023-01-30 20:54:18 -0400

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x in range(len(nums)):
-            for y in range(len(nums)):
-                if x == y:
-                    continue
-                elif nums[x] + nums[y] == target:
-                    return [x,y]
+        hashmap = dict()
+
+        for i, j in enumerate(nums):
+            diff = target - j
+            if diff in hashmap:
+                return hashmap[diff], i
+            hashmap[j] = i
+
+# BRUTE FORCE
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for x in range(len(nums)):
+#             for y in range(len(nums)):
+#                 if x == y:
+#                     continue
+#                 elif nums[x] + nums[y] == target:
+#                     return [x,y]
